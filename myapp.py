3316ae656fccbeec478b03b610f63029008f5a9f
Yoni Gottesman <yonigo10@gmail.com>
2015-11-04 19:22:37 +0200

    added bootsrap and html

--- myapp.py
+++ myapp.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
+#    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)
