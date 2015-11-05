fc5ab6dd74a937829753bc4e0d21d97a43d37367
Yoni Gottesman <yonigo10@gmail.com>
2015-11-05 17:22:43 +0200

    Added basic routes

--- myapp.py
+++ myapp.py
@@ -5,9 +5,22 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return render_template('index.html')
-#    return "Hello World!"
+def smartHome():
+    return render_template('index.html', index_active="active")
+
+@app.route("/lights")
+def lights():
+    return render_template('lights.html', lights_active="active")
+
+@app.route("/ac")
+def ac():
+    return render_template('ac.html', ac_active="active")
+
+@app.route("/boiler")
+def boiler():
+    return render_template('boiler.html', boiler_active="active")
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=True)
